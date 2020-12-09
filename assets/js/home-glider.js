2053255c159d6a016bba7502842ab5bcb7fa2a63
Martha Hipley <info@marthahipley.com>
2020-12-09 13:47:30 -0500

    fixing load bug with a callback

--- assets/js/home-glider.js
+++ assets/js/home-glider.js
@@ -1,36 +1,42 @@
 window.addEventListener('load', function(){
-    $("img").addClass('hasloaded');
-    new Glider(document.querySelector('.glider'), {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        draggable: true,
-        scrollLock: true,
-        arrows: {
-          prev: '.glider-prev',
-          next: '.glider-next'
-        },
-        responsive: [
 
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2.5,
-                    slidesToScroll: 1,
-                    duration: 2,
-                    scrollLock: false
-                }
-            }
-        ]
-      });
+      function runGlider(callback) {
 
-    //   new Glider(document.querySelector('.project-glider'), {
-    //     new Glider(document.querySelector('.glider'), {
-    //         slidesToShow: 1,
-    //         draggable: true,
-    //         arrows: {
-    //           prev: '.glider-prev',
-    //           next: '.glider-next'
-    //         }
-    //       });
-    //   });
+        new Glider(document.querySelector('.glider'), {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          draggable: true,
+          scrollLock: true,
+          arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
+          },
+          responsive: [
+  
+              {
+                  breakpoint: 768,
+                  settings: {
+                      slidesToShow: 2.5,
+                      slidesToScroll: 1,
+                      duration: 2,
+                      scrollLock: false
+                  }
+              }
+          ]
+        });
+        console.log(1);
+        callback();
+      }
+
+    function showCards() {
+        console.log(2);
+        var cards = document.getElementsByClassName("post-card");
+        [].forEach.call(cards, function(el) {
+            el.classList.add("show");
+            el.classList.remove("hide");
+        });
+    }
+
+    runGlider(showCards);
+
   })
